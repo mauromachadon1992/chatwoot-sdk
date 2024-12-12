18d76c3c186ed65e7c357725ecc1d634bb1eff16
mauromachadon1992 <mauro.machadon1992@gmail.com>
2024-12-12 20:08:41 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -58,97 +58,123 @@ export type { webhook } from "./models/webhook";
 export type { webhook_create_update_payload } from "./models/webhook_create_update_payload";
 
 import { AccountAgentBots } from "./services/AccountAgentBots";
-import { Accounts } from "./services/platform/Accounts";
-import { AccountUsers } from "./services/platform/AccountUsers";
-import { AgentBots } from "./services/platform/AgentBots";
 import { Agents } from "./services/Agents";
 import { AutomationRule } from "./services/AutomationRules";
 import { CannedResponses } from "./services/CannedResponses";
+import { ContactsApi } from "./services/client/Contacts";
+import { ConversationsApi } from "./services/client/Conversations";
+import { MessagesApi } from "./services/client/Messages";
 import { Contact } from "./services/ContactInboxes";
 import { Contacts } from "./services/Contacts";
-import { ContactsApi } from "./services/client/Contacts";
 import { ConversationAssignment } from "./services/ConversationAssignments";
 import { ConversationLabels } from "./services/ConversationLabels";
 import { Conversations } from "./services/Conversations";
-import { ConversationsApi } from "./services/client/Conversations";
 import { CustomAttributes } from "./services/CustomAttributes";
 import { CustomFilters } from "./services/CustomFilters";
 import { Inboxes } from "./services/Inboxes";
 import { Integrations } from "./services/Integrations";
 import { Messages } from "./services/Messages";
-import { MessagesApi } from "./services/client/Messages";
+import { Accounts } from "./services/platform/Accounts";
+import { AccountUsers } from "./services/platform/AccountUsers";
+import { AgentBots } from "./services/platform/AgentBots";
+import { Users } from "./services/platform/Users";
 import { Profile } from "./services/Profile";
 import { Reports } from "./services/Reports";
 import { Teams } from "./services/Teams";
-import { Users } from "./services/platform/Users";
 import { Webhooks } from "./services/Webhooks";
 
 import { ChatwootAPIConfig } from "./core/ChatwootAPI";
 
-export default class ChatwootClient {
-    private chatwootAPI: ChatwootAPIConfig;
-    public readonly client;
-    public readonly platform;
+export interface ChatwootClientConfig extends ChatwootAPIConfig {
+    baseUrl: string;
+    accountId?: number;
+    apiAccessToken: string;
+    inboxIdentifier?: string;
+    contactIdentifier?: string;
+    websiteToken?: string;
+}
 
-    constructor({ config }: { config: ChatwootAPIConfig }) {
-        this.chatwootAPI = config;
+export class ChatwootClient {
+    private chatwootAPI: ChatwootAPIConfig;
+
+    public readonly client: {
+        contacts: ContactsApi;
+        conversations: ConversationsApi;
+        messages: MessagesApi;
+    };
+
+    public readonly platform: {
+        accounts: Accounts;
+        accountUsers: AccountUsers;
+        agentBots: AgentBots;
+        users: Users;
+    };
+
+    public readonly accountAgentBots: AccountAgentBots;
+    public readonly agentBots: AgentBots;
+    public readonly agents: Agents;
+    public readonly automationRule: AutomationRule;
+    public readonly cannedResponses: CannedResponses;
+    public readonly contact: Contact;
+    public readonly contacts: Contacts;
+    public readonly customAttributes: CustomAttributes;
+    public readonly conversationAssignment: ConversationAssignment;
+    public readonly conversationLabels: ConversationLabels;
+    public readonly conversations: Conversations;
+    public readonly customFilters: CustomFilters;
+    public readonly inboxes: Inboxes;
+    public readonly integrations: Integrations;
+    public readonly messages: Messages;
+    public readonly profile: Profile;
+    public readonly reports: Reports;
+    public readonly teams: Teams;
+    public readonly users: Users;
+    public readonly webhooks: Webhooks;
+
+    constructor({ config }: { config: ChatwootClientConfig }) {
+        this.chatwootAPI = {
+            basePath: config.baseUrl,
+            with_credentials: config.with_credentials,
+            credentials: config.credentials,
+            token: config.apiAccessToken,
+            username: config.username,
+            password: config.password,
+            headers: config.headers,
+            encode_path: config.encode_path
+        };
 
         this.client = {
-            contacts: new ContactsApi({ config: config }),
-            conversations: new ConversationsApi({ config: config }),
-            messages: new MessagesApi({ config: config }),
+            contacts: new ContactsApi({ config: this.chatwootAPI }),
+            conversations: new ConversationsApi({ config: this.chatwootAPI }),
+            messages: new MessagesApi({ config: this.chatwootAPI }),
         };
 
         this.platform = {
-            accounts: new Accounts({ config: config }),
-            accountUsers: new AccountUsers({ config: config }),
-            agentBots: new AgentBots({ config: config }),
-            users: new Users({ config: config }),
+            accounts: new Accounts({ config: this.chatwootAPI }),
+            accountUsers: new AccountUsers({ config: this.chatwootAPI }),
+            agentBots: new AgentBots({ config: this.chatwootAPI }),
+            users: new Users({ config: this.chatwootAPI }),
         };
 
-        this.accountAgentBots = new AccountAgentBots({ config: config });
-
-        this.agentBots = new AgentBots({ config: config });
-        this.agents = new Agents({ config: config });
-        this.automationRule = new AutomationRule({ config: config });
-        this.cannedResponses = new CannedResponses({ config: config });
-        this.contact = new Contact({ config: config });
-        this.contacts = new Contacts({ config: config });
-
-        this.customAttributes = new CustomAttributes({ config: config });
-        this.conversationAssignment = new ConversationAssignment({ config: config });
-        this.conversationLabels = new ConversationLabels({ config: config });
-        this.conversations = new Conversations({ config: config });
-        this.customFilters = new CustomFilters({ config: config });
-        this.inboxes = new Inboxes({ config: config });
-        this.integrations = new Integrations({ config: config });
-        this.messages = new Messages({ config: config });
-
-        this.profile = new Profile({ config: config });
-        this.reports = new Reports({ config: config });
-        this.teams = new Teams({ config: config });
-        this.users = new Users({ config: config });
-        this.webhooks = new Webhooks({ config: config });
+        this.accountAgentBots = new AccountAgentBots({ config: this.chatwootAPI });
+        this.agentBots = new AgentBots({ config: this.chatwootAPI });
+        this.agents = new Agents({ config: this.chatwootAPI });
+        this.automationRule = new AutomationRule({ config: this.chatwootAPI });
+        this.cannedResponses = new CannedResponses({ config: this.chatwootAPI });
+        this.contact = new Contact({ config: this.chatwootAPI });
+        this.contacts = new Contacts({ config: this.chatwootAPI });
+        this.customAttributes = new CustomAttributes({ config: this.chatwootAPI });
+        this.conversationAssignment = new ConversationAssignment({ config: this.chatwootAPI });
+        this.conversationLabels = new ConversationLabels({ config: this.chatwootAPI });
+        this.conversations = new Conversations({ config: this.chatwootAPI });
+        this.customFilters = new CustomFilters({ config: this.chatwootAPI });
+        this.inboxes = new Inboxes({ config: this.chatwootAPI });
+        this.integrations = new Integrations({ config: this.chatwootAPI });
+        this.messages = new Messages({ config: this.chatwootAPI });
+        this.profile = new Profile({ config: this.chatwootAPI });
+        this.reports = new Reports({ config: this.chatwootAPI });
+        this.teams = new Teams({ config: this.chatwootAPI });
+        this.users = new Users({ config: this.chatwootAPI });
+        this.webhooks = new Webhooks({ config: this.chatwootAPI });
     }
-
-    public accountAgentBots: AccountAgentBots;
-    public agentBots: AgentBots;
-    public agents: Agents;
-    public automationRule: AutomationRule;
-    public cannedResponses: CannedResponses;
-    public contact: Contact;
-    public contacts: Contacts;
-    public conversationAssignment: ConversationAssignment;
-    public conversationLabels: ConversationLabels;
-    public conversations: Conversations;
-    public customAttributes: CustomAttributes;
-    public customFilters: CustomFilters;
-    public inboxes: Inboxes;
-    public integrations: Integrations;
-    public messages: Messages;
-    public profile: Profile;
-    public reports: Reports;
-    public teams: Teams;
-    public users: Users;
-    public webhooks: Webhooks;
 }
